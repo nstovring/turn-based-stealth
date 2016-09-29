94240347870868243d588fce91a34f0189099a7d
nstovring <nstovring@live.dk>
2016-09-29 12:25:21 +0200

    Action Queue and Door Complete, Guard in progress

---
+++ Assets/_scripts/Guard.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class Guard : Character
+{
+    private bool conscious = true;
+    private bool stunned = false;
+
+    public enum GuardState {Conscious, Unconscious, Stunned, Alert};
+    public GuardState MyGuardState = GuardState.Conscious;
+    struct Inventory
+    {
+        int coin;
+        bool key;
+
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void ChangeState(GuardState newState)
+    {
+        MyGuardState = newState;
+    }
+
+    public IEnumerator GetBlackJacked()
+    {
+        yield return new WaitForSeconds(1f);
+        ChangeState(GuardState.Unconscious);
+    }
+
+    IEnumerator GetPickPocketed()
+    {
+        return null;
+    }
+}
