c68e6ad0ed55c69433e63cfeef08f49b00ccfd60
JeppeKorsholm <jeppe.korsholm@hotmail.com>
2016-11-08 15:16:59 +0100

    MainMenu added

---
+++ Assets/_scripts/LevelSelect.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelect : MonoBehaviour { 
+
+    public bool _showGui;
+    public float FadeTime;
+
+    public Text NodeName;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	    _showGui = false;
+	    NodeName.color = Color.clear;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    
+	    HoverSelect();
+
+	    FadeText();
+
+	}
+
+    
+
+    void HoverSelect()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+
+            if (hit.collider.tag == "MapNode")
+            {
+                _showGui = true;
+            }
+            else
+            {
+                _showGui = false;
+            }
+
+        }
+    }
+
+
+    void FadeText()
+    {
+
+        if (_showGui)
+        {
+            NodeName.color = Color.Lerp(NodeName.color, Color.black, FadeTime*Time.deltaTime);
+        }
+        else
+        {
+            NodeName.color = Color.Lerp(NodeName.color, Color.clear, FadeTime*Time.deltaTime);
+        }
+    }
+
+}
